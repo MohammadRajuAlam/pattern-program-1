aafdb213155f5f37a6d210bb1ab7e99aeaac47b2
Mohammad Raju Alam <66439087+MohammadRajuAlam@users.noreply.github.com>
2020-12-12 15:52:15 +0530

    Update patternprogram1.py

--- patternprogram1.py
+++ patternprogram1.py
@@ -1,1 +1,38 @@
+# program 1. wap to display 
+             * * * * *
+             * * * * *
+             * * * * *
+             * * * * *  
 
+# 1 solution using for loop
+ 
+num=int(input("Enter rows:"))
+
+for i in range(1,num+1):
+    print("* "*num, end="")
+    print()
+
+# 2 salution
+
+def pt(a,b):
+    for i in range(1,rows+1):
+        for j in range(1,colm+1):
+            print("*", end=" ")
+        print()
+
+rows=int(input("Enter no of rows:"))
+colm=int(input("Enter no of colmns:"))
+
+pt(rows,colm)
+
+# 3 using while loop with fun
+
+def pt(a):
+    s=1
+    while a>=s:
+       print("* "*a)
+       s=s+1
+    print()
+num=int(input("Ente no of rows: "))
+
+pt(num)

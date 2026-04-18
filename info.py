d833c271404965861f5caa8235892e89baa51dca
mdrajualam <raju.datascientist5@gmail.com>
2026-04-18 19:08:10 +0530

    modified the info code

--- info.py
+++ info.py
@@ -1,7 +1,7 @@
 # Wap to dispay your infomation, take input from user in user define function 
 
-def info(name, age,address,phone,email,qual,gender): 
-    print(f"Name: {name}\nAge: {age}\naddress: {address}\nPhone No: {phone}\nEmail: {email}\nQualification: {qual}\nGender: {gender}")
+def info(name, age,address,phone,email,qual,gender,marrage_status): 
+    print(f"Name: {name}\nAge: {age}\naddress: {address}\nPhone No: {phone}\nEmail: {email}\nQualification: {qual}\nGender: {gender}\nMarrage Status: {marrage_status}")
     
 Name=input("Enter your name:")
 Age=int(input("Enter Your age:"))
@@ -10,5 +10,6 @@ Phone=int(input("Enter your phone No:"))
 Email=input("Enter your Email Id:")
 Q=input("Enter you Qualification:")
 Sex=input("Enter your Gender:")
+M=input("Enter your Marrage Status:")
 
-info(Name, Age,Add,Phone,Email,Q,Sex)
+info(Name, Age,Add,Phone,Email,Q,Sex,M)
